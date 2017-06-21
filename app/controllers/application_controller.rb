c0c76389c9f92559d1c258da037133bde9ce6890
violake <violake@foxmail.com>
2017-06-21 15:36:07 +1000

    heroku update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-    render html: "¡Hola, mundo!"
+    render html: "promote world!"
   end
 end
